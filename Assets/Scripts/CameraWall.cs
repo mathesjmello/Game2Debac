253ab3928c3909fb095e71cd8c3944359df93211
matheus junqueira de mello <33270120+mathesjmello@users.noreply.github.com>
2023-04-20 10:58:13 -0300

    adding pg

---
+++ Assets/Scripts/CameraWall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWall : MonoBehaviour
+{
+
+private Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
